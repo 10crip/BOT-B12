f651441c5fea2e7ae11819002399c96598d09097
10crip <kbsuicidajr@gmail.com>
2026-04-08 00:58:54 -0300

    Create setadmup.js

---
+++ commands/setadmup.js
@@ -0,0 +1,33 @@
+const { updateGuildConfig } = require('../guildConfig');
+
+module.exports = {
+    name: 'setadmup',
+
+    async execute(message) {
+        if (!message.guild) return;
+
+        if (!message.member.permissions.has('Administrator')) {
+            return message.reply('❌ Você precisa ser administrador para usar este comando.')
+                .then(msg => setTimeout(() => msg.delete().catch(() => {}), 10000))
+                .catch(() => {});
+        }
+
+        const cargos = [...message.mentions.roles.values()];
+
+        if (!cargos.length) {
+            return message.reply(`❌ Use assim: \`${process.env.PREFIX || '!'}setadmup @cargo1 @cargo2 @cargo3\``)
+                .then(msg => setTimeout(() => msg.delete().catch(() => {}), 12000))
+                .catch(() => {});
+        }
+
+        updateGuildConfig(message.guild.id, {
+            admUpRoleIds: cargos.map(cargo => cargo.id)
+        });
+
+        await message.reply(`✅ Cargos de promoção/rebaixamento definidos como: ${cargos.join(', ')}`)
+            .then(msg => setTimeout(() => msg.delete().catch(() => {}), 12000))
+            .catch(() => {});
+
+        await message.delete().catch(() => {});
+    }
+};

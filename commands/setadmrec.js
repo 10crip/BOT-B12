175f363535925492c71ad8b97ebfbd583439900b
10crip <kbsuicidajr@gmail.com>
2026-04-07 22:58:29 -0300

    Update setadmrec.js

--- commands/setadmrec.js
+++ commands/setadmrec.js
@@ -12,20 +12,20 @@ module.exports = {
                 .catch(() => {});
         }
 
-        const cargo = message.mentions.roles.first();
+        const cargos = [...message.mentions.roles.values()];
 
-        if (!cargo) {
-            return message.reply(`❌ Use assim: \`${process.env.PREFIX || '!'}setadmrec @cargo\``)
-                .then(msg => setTimeout(() => msg.delete().catch(() => {}), 10000))
+        if (!cargos.length) {
+            return message.reply(`❌ Use assim: \`${process.env.PREFIX || '!'}setadmrec @cargo1 @cargo2 @cargo3\``)
+                .then(msg => setTimeout(() => msg.delete().catch(() => {}), 12000))
                 .catch(() => {});
         }
 
         updateGuildConfig(message.guild.id, {
-            admRecRoleId: cargo.id
+            admRecRoleIds: cargos.map(cargo => cargo.id)
         });
 
-        await message.reply(`✅ Cargo de admrec salvo como ${cargo}.`)
-            .then(msg => setTimeout(() => msg.delete().catch(() => {}), 10000))
+        await message.reply(`✅ Cargos da transferência definidos como: ${cargos.join(', ')}`)
+            .then(msg => setTimeout(() => msg.delete().catch(() => {}), 12000))
             .catch(() => {});
 
         await message.delete().catch(() => {});

be1b8be986838d9a232814958877df616e4f4492
10crip <kbsuicidajr@gmail.com>
2026-04-13 00:23:58 -0300

    Update painelponto.js

--- commands/painelponto.js
+++ commands/painelponto.js
@@ -29,7 +29,7 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setColor('#2B2D31')
-            .setTitle('BATE-PONTO B12')
+            .setTitle('BATE-PONTO YKZ')
             .setDescription('CLIQUE NO BOTAO ABAIXO PARA INICIAR SEU BATE-PONTO');
 
         const row = new ActionRowBuilder().addComponents(
